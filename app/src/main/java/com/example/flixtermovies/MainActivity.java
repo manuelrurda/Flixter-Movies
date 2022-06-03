ac5869d542b7a11bc094a5928b313c22be87d8e1
manuelrurda <manuelrurda@gmail.com>
2022-06-03 09:15:35 -0700

    First and Second Video: Day1
    Project setup. Added codepath dependencies. Made first API request to movie db. Request was successful. Created movie class and fectched the movie data form the api. Created movie objects with the fromJsonarray method. Started implementing RecyclerLayout and movie item layout.

--- app/src/main/java/com/example/flixtermovies/MainActivity.java
+++ app/src/main/java/com/example/flixtermovies/MainActivity.java
@@ -3,12 +3,54 @@ package com.example.flixtermovies;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+import com.example.flixtermovies.models.Movie;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
+
+import okhttp3.Headers;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String API_KEY = "c008fd1948ccc56003222c66c1eefe0d";
+    private static final String NOW_PLAYING_URL = String.format("https://api.themoviedb.org/3/movie/now_playing?api_key=%s", API_KEY);
+    private static final String TAG = "Main Activity";
+
+    List<Movie> movies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AsyncHttpClient client = new AsyncHttpClient();
+
+        client.get(NOW_PLAYING_URL, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                JSONObject jsonObject = json.jsonObject;
+                try {
+                    JSONArray results = jsonObject.getJSONArray("results");
+                    Log.d(TAG, "Results: " + results);
+                    movies = Movie.fromJsonArray(results);
+                    Log.d(TAG, "Movies: " + movies.size());
+                } catch (JSONException e) {
+                    Log.d(TAG, "Error, json exception: ", e);
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+
+            }
+        });
     }
 }
