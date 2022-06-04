6850de51425177c0d9785b7368decc1bfe42aa46
manuelrurda <manuelrurda@gmail.com>
2022-06-04 15:55:11 -0700

    Improved Portrait UI.
    Added custom action bar. Changed font. Added item shadow. Justified text and centered content.

--- app/src/main/java/com/example/flixtermovies/adapters/MovieAdapter.java
+++ app/src/main/java/com/example/flixtermovies/adapters/MovieAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.view.LayoutInflater;
+import android.view.RoundedCorner;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -13,6 +14,7 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
 import com.example.flixtermovies.MovieDetails;
 import com.example.flixtermovies.R;
 import com.example.flixtermovies.models.Movie;
@@ -79,13 +81,15 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
             String imageUrl = (context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT)
                     ? movie.getPosterPath() : movie.getBackdropPath();
 
+            int radius = 20;
             Glide.with(context)
                     .load(imageUrl)
                     .placeholder(R.drawable.flicks_movie_placeholder)
+                    .transform(new RoundedCorners(radius))
                     .into(moviePoster);
 
 
-        }x
+        }
 
         @Override
         public void onClick(View v) {
